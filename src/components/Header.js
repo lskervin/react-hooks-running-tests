34a14d540fd8d3fd926b522cc0337cc900da50a5
LeightonSkervin <lskervin23@gmail.com>
2024-02-22 12:49:21 -0500

    lab complete

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,10 @@
 import React from "react";
 
-function Header(props) {
+function Header() {
       return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
+        <h1>hello from the Header!</h1>
+          
+      );
 }
 
 export default Header;
